4aa77bf6a2bf7f1abc3450b7fd58a667a121721a
agctute <cjyoung2001@yahoo.com>
2022-06-21 20:49:32 -0700

    Created a method for defining value given a food and a target pet

--- myAI/functions.py
+++ myAI/functions.py
@@ -1,7 +1,7 @@
 import sapai.shop as sh
 import random as rand
 import numpy as np
-
+import math
 
 # This function returns a list containing the mean, median, and maximum values of pets on a given turn
 # pet_values: a 66-element dictionary containing the values of each pet by name
@@ -17,7 +17,7 @@ def turn_stats(pet_values, turn):
 # generates a 5x5 matrix of values for the gold function
 # rows: # of empty spaces
 # columns: # of gold: 0 - 3, 1 - 6, 2 - 9, 3 - 4 or 5, 4 - 7 or 8, 5 - 10
-def generate_threshold(empty_spaces, gold):
+def generate_threshold():
     parameters = np.empty(6, 6)
     parameters[0, 1] = rand.uniform(0, 1)
     parameters[0, 2] = rand.uniform(0, parameters[0, 1])
@@ -51,3 +51,23 @@ def threshold_function(parameters, empty_spaces, gold):
         10: parameters[empty_spaces, 5],
     }
     return switch.get(gold)
+
+
+# Defines the function that determines the value of a food given a specific pet
+# the value function will be chosen between:
+# sigmoid, gaussian
+# Variables:
+# function: denotes which function to calculate the value with
+# food_type: denotes if the food is given to a specific pet or distributed randomly
+# min_max: the range of values that the food can be
+# pet_stats: the sum of the att and hp of one pet
+def food_value(function, min_max, pet_stats, coeff):
+    # first we normalize the stats to work with the function
+    pet_stats -= 50
+    pet_stats = pet_stats/50
+    switch = {
+        "sigmoid": 1 / (1 + math.exp(-coeff*pet_stats)),
+        "gaussian": math.exp(-coeff*pet_stats**2)
+    }
+    return min_max[0] + min_max[1]*switch.get(function)
+
